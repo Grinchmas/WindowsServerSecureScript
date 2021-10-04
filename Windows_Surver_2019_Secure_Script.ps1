1e6ce3925f344eae58464d8b3c84d4f59ea3f749
Grinchmas <90066636+Grinchmas@users.noreply.github.com>
2021-10-04 16:21:12 -0500

    Add files via upload

---
+++ Windows_Surver_2019_Secure_Script.ps1
@@ -0,0 +1,39 @@
+
+#This is a Scipt that was written by Grinchmas
+
+#sets the admin and execution policy from 
+
+#262588213843476. "Self Elevating PowerShell Script or Administrator Rights Required." Gist, #gist.github.com/atao/a103e443ffb37d5d0f0e7097e4342a28. 
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }; 
+
+#Changes Admin Password
+Net user "Administrator" $in = Read-Host "Enter_Admin_Pwd";
+$in = "NiceTry `\_(^_^)_/`";
+
+#Messes with Firewall (non GPO)
+Set-NetFirewallProfile -All -Enabled True;
+Set-NetFirewallProfile -Name ('Public','Private','Domain') -DefaultInboundAction Block;
+Set-NetFirewallProfile -Name ('Public','Private','Domain') -DefaultOutboundAction Allow;
+
+#Blocks Bad Ports (non GPO) (check the last port)
+$BadPorts = Read-Host "('1'..'12'+'23..24'+'26'..'52'+'81'..'109'+'111'..'442'+'444'..'7999'+'8001'..'65535')"
+New-NetFirewallRule -DisplayName "BlockOutboundBadPortsTCP" -Profile ('Domain','Private','Public') -Direction Outbound -Action Block -Protocol TCP -Local Port $BadPorts;
+New-NetFirewallRule -DisplayName "BlockInboundBadPortsTCP" -Profile ('Domain','Private','Public') -Direction Inbound -Action Block -Protocol TCP -Local Port $BadPorts;
+New-NetFirewallRule -DisplayName "BlockOutboundBadPortsUDP" -Profile ('Domain','Private','Public') -Direction Outbound -Action Block -Protocol UDP -Local Port $BadPorts;
+New-NetFirewallRule -DisplayName "BlockInboundBadPortsUDP" -Profile ('Domain','Private','Public') -Direction Inbound -Action Block -Protocol UDP -Local Port $BadPorts;
+
+#Removes Bad GPOs
+Get-GPO -All;
+$BadGPO = Read-Host "EnterBadGPONameAfterDocumentation";
+Remove-GPO $BadGPO;
+
+#other thing to secure Windows
+Disable-PSRemoting;
+Stop-Service -Name ("bthserv","MapsBroker","Spooler","SSDPSRV");
+
+#opens host file
+start C:\Windows\System32\drivers\etc\hosts;
+
+#end Powershell
+Exit;
